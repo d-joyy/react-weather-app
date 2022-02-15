30a5dfd692a8e384166d7415857c702f4a21edf4
Danielle <daniellejoyy91@gmail.com>
2022-02-14 19:48:05 -0500

    Edited title and link

--- src/App.js
+++ src/App.js
@@ -17,9 +17,7 @@ function App() {
           Open-sourced on GitHub
         </a>{" "}
         and hosted on{" "}
-        <a href="https://app.netlify.com/sites/zen-swanson-1c04e2/deploys">
-          Netlify.
-        </a>
+        <a href="https://zen-swanson-1c04e2.netlify.app">Netlify.</a>
       </footer>
 
       <script src="src/app.js"></script>
