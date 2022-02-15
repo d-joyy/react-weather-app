02fe5f365745ac3e6582e83735c91d2ef37c5b7c
Danielle <daniellejoyy91@gmail.com>
2022-02-14 19:53:32 -0500

    Updated link

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
         <a href="https://github.com/d-joyy/react-weather-app">
           Open-sourced on GitHub
         </a>{" "}
-        and hosted on{" "}
-        <a href="https://zen-swanson-1c04e2.netlify.app">Netlify.</a>
+        and hosted on <a href="https://app.netlify.com">Netlify.</a>
       </footer>
 
       <script src="src/app.js"></script>
