c54a9230ee4f471fb862a9591fee1d6a075770d2
Danielle <daniellejoyy91@gmail.com>
2022-02-23 00:41:13 -0500

    Edited GitHub URL

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <Weather defaultCity="Japan" />
         <footer>
           This project was coded by Danielle and is{" "}
-          <a href="https://github.com/d-joyy/react-weather-app" target="_blank">
+          <a href="https://github.com/d-joyy/react-weather-app">
             open-sourced on GitHub
           </a>{" "}
           and hosted on <a href="https://app.netlify.com">Netlify.</a>
